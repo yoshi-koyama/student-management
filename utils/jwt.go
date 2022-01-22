634ec75ecf7e37ff80ecc90fb5ecdbe5e2f14c78
yoshi-koyama <koyama@reytech.co.jp>
2022-01-22 19:26:25 +0900

    added jwt authentication logic module

---
+++ utils/jwt.go
@@ -0,0 +1,41 @@
+package utils
+
+import (
+	"fmt"
+	"github.com/golang-jwt/jwt"
+)
+
+var SecretKey = ""
+var STUDENT_ID = "student_id"
+
+func CreateToken(studentId int64, ) (string, error){
+	claim := jwt.MapClaims{}
+	claim[STUDENT_ID] = studentId
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claim)
+	signedToken, err := token.SignedString([]byte(SecretKey))
+	if err != nil {
+		return "", fmt.Errorf("cannot generate token")
+	}
+
+	return signedToken, nil
+}
+
+func ValidateToken(value string) (jwt.MapClaims, error) {
+	token, err := jwt.Parse(value, func(t *jwt.Token) (interface{}, error) {
+		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method %v", t.Header["alg"])
+		}
+		return []byte(SecretKey), nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	if claim, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		return claim, nil
+	} else {
+		return nil, err
+	}
+
+}
